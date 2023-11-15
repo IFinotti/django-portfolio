7a1581e2d52588e892c87587974bfc5c4c83a8b9
Isaac Finotti <imisaac2807@gmail.com>
2023-11-15 15:19:43 -0300

    dotenv files

--- project/settings.py
+++ project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xy9+z5ha(ae6+q#c6famcv=s2n#lpbphbfz9&yv5a)+9k&2o_*'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
