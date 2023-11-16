24a028e0740acae7e42975eaa7e81c154128c3c9
Isaac Finotti <imisaac2807@gmail.com>
2023-11-16 20:10:40 -0300

    Urls and views

--- portfolio/views.py
+++ portfolio/views.py
@@ -2,9 +2,7 @@ from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
 
-# Create your views here.
-
-
 class Home(View):
-    def get(self, *args, **kwargs):
+    def get(self, request, *args, **kwargs):
         return HttpResponse('Home')
+
