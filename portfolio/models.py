db6f4b206528d57a1d3cf9b1805a48f9c85156bf
Isaac Finotti <imisaac2807@gmail.com>
2023-11-16 20:15:47 -0300

    models of projects

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Projects(models.Model):
+    name = models.CharField(max_length=25)
+    short_description = models.TextField(max_length=255)
+    long_description = models.TextField()
+    image = models.ImageField(upload_to='project_images/%Y/%m')
+    slug = models.SlugField(unique=True, blank=True, null=True)
+    
